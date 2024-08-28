140f1907ab4d368948fbdf2150743f25cb6654d1
Yohannes Walle <etawe1627@gmail.com>
2024-08-27 15:28:54 -1200

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,20 @@ require('colors');
 require('dotenv').config();
 
 const app = express();
-// const cors = require('cors');
+const cors = require('cors'); // Import the cors package
 const connectDB = require('./config/connectDB');
 const userRoutes = require('./routes/user.routes');
 const authRoutes = require('./routes/auth.routes');
 const restaurantRoutes = require('./routes/restaurant.routes');
-const menuRoutes = require('./routes/menu.routes')
-const reviewRoutes = require('./routes/review.routes')
+const menuRoutes = require('./routes/menu.routes');
+const reviewRoutes = require('./routes/review.routes');
 const cartRoutes = require('./routes/cart.routes');
 const orderRoutes = require('./routes/order.routes');
 const driverRoutes = require('./routes/driver.routes');
 
 // middleware
 app.use(express.json());
-// app.use(cors());
+app.use(cors()); // Enable CORS
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/restaurants', restaurantRoutes);
@@ -29,7 +29,7 @@ app.use('/api/driver', driverRoutes);
 //connecting to the database
 connectDB();
 
-// listening the server
+// listening to the server
 
 const port = process.env.PORT || 5000;
 
