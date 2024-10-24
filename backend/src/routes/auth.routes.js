2c10c484789026fe2b72c3dec54d73e000db09d8
Yohannes Walle <etawe1627@gmail.com>
2024-10-24 11:24:01 -1200

    authlimmiter path modified

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // const { authMiddleware } = require('../Middleware/authMiddleware');
-const { authLimiter } = require('../Middleware/auth.limiter');
+const { authLimiter } = require('../middleware/auth.limiter');
 
 
 const {
